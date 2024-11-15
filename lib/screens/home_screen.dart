53f3c3162e1d54952207de69c6a2b077a9328812
piro-piyush <piyush72717272@gmail.com>
2024-11-15 21:56:54 +0530

    fixes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,59 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:senpai_dex/screens/animes_screen.dart';
+import '/screens/animes_screen.dart';
+import '/screens/categories_screen.dart';
+import '/screens/search_screen.dart';
+import '/screens/settings_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, this.index});
+  const HomeScreen({
+    super.key,
+    this.index,
+  });
+
   final int? index;
 
   static const routeName = '/home';
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  @override
-  void initState() {
-    if (widget.index != null) {
-      _selectedIndex = widget.index!;
-    }
-    super.initState();
-  }
-
   final _destinations = [
-    const NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-    const NavigationDestination(icon: Icon(Icons.search), label: "Search"),
-    const NavigationDestination(icon: Icon(Icons.category), label: "Category"),
-    const NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+    const NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+    const NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+    const NavigationDestination(
+        icon: Icon(Icons.category), label: 'Categories'),
+    const NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
   ];
 
   final _screens = const [
     AnimesScreen(),
-    Center(
-      child: Text("Search Screen"),
-    ),
-    Center(
-      child: Text("Category Screen"),
-    ),
-    Center(
-      child: Text("Settings Screen"),
-    ),
+    SearchScreen(),
+    CategoriesScreen(),
+    SettingsScreen(),
   ];
+
+  int _currentScreenIndex = 0;
+
+  @override
+  void initState() {
+    if (widget.index != null) {
+      _currentScreenIndex = widget.index!;
+    }
+    super.initState();
+  }
+
+  bool canPop() {
+    if (_currentScreenIndex == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool onPopInvoked(bool didPop) {
+    if (!didPop) {
+      _currentScreenIndex = 0;
+      setState(() {});
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _selectedIndex,
-        elevation: 12,
-        backgroundColor: Theme.of(context).dialogBackgroundColor,
-        destinations: _destinations,
-        onDestinationSelected: (value) {
-          setState(() {
-            _selectedIndex = value;
-          });
-        },
+    return PopScope(
+      onPopInvoked: onPopInvoked,
+      canPop: canPop(),
+      child: Scaffold(
+        body: _screens[_currentScreenIndex],
+        bottomNavigationBar: NavigationBar(
+          elevation: 12,
+          destinations: _destinations,
+          animationDuration: const Duration(milliseconds: 200),
+          backgroundColor: Theme.of(context).dialogBackgroundColor,
+          selectedIndex: _currentScreenIndex,
+          onDestinationSelected: (value) {
+            setState(() {
+              _currentScreenIndex = value;
+            });
+          },
+        ),
       ),
     );
   }
---
+++ lib/views/anime_grid_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:senpai_dex/api/get_anime_by_ranking_type.dart';
+import 'package:senpai_dex/core/screens/error_screen.dart';
+import 'package:senpai_dex/core/widgets/loader.dart';
+import 'package:senpai_dex/models/anime_category.dart';
+import 'package:senpai_dex/views/animes_grid_list.dart';
+
+class AnimeGridView extends StatelessWidget {
+  const AnimeGridView({
+    super.key,
+    required this.category,
+  });
+
+  final AnimeCategory category;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getAnimeByRankingTypeApi(
+        rankingType: category.rankingType,
+        limit: 100,
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Loader();
+        }
+
+        if (snapshot.data != null) {
+          final animes = snapshot.data;
+
+          return AnimesGridList(
+            title: category.title,
+            animes: animes!,
+          );
+        }
+
+        return ErrorScreen(error: snapshot.error.toString());
+      },
+    );
+  }
+}
